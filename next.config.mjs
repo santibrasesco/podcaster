87247c6a8a4fcde96b8d3e30a2ef36b70bda24fb
Santiago <santibrasesco@gmail.com>
2024-09-02 17:48:47 +0200

    feat: Add spinner and styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "is1-ssl.mzstatic.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
